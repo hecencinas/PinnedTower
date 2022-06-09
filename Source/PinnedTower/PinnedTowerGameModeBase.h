30760eb61f934995461b5d997a7dcddddcebe358
Hector Gabriel Encinas <hector.gab.encinas@gmail.com>
2022-06-08 22:44:20 -0300

    Base project

---
+++ Source/PinnedTower/PinnedTowerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PinnedTowerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINNEDTOWER_API APinnedTowerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
